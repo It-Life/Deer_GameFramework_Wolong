e33da938045a3355ddb5afdb35715f1a88f48f46
onion <652597991@qq.com>
2022-07-24 22:23:46 +0800

    重命名

--- CommitResources/CommitResources_100.bat
+++ CommitResources/CommitResources_100.bat
@@ -2,6 +2,6 @@
 
 cd /d %~dp0/% 
 
-rsync -avzP --port=873 --progress --delete --password-file=pass/rsyncd.secrets Huatuo_100 bundles@121.4.195.168::bundles
+rsync -avzP --port=873 --progress --delete --password-file=pass/rsyncd.secrets Wolong_100 bundles@121.4.195.168::bundles
 
 pause
--- LubanTools/DesignerConfigs/StreamingAssets_BuildConfig_Wolong.bat
+++ LubanTools/DesignerConfigs/StreamingAssets_BuildConfig_Wolong.bat
@@ -2,17 +2,17 @@
 
 cd /d %~dp0
 set ROOT_PATH=%~dp0
-set WORK_NUMBER=Huatuo_100
+set WORK_NUMBER=StreamingAssets
 set PLATFORM=Windows64
 set DATA_OUTPUT=%ROOT_PATH%..\GenerateDatas\LubanConfig
-set GAME_COMMIT_PATH=%ROOT_PATH%..\..\CommitResources
-set DATA_UPLOAD_OUTPUT=%GAME_COMMIT_PATH%\%WORK_NUMBER%\%PLATFORM%\LubanConfig
+set GAME_COMMIT_PATH=%ROOT_PATH%..\..\Assets
+set DATA_UPLOAD_OUTPUT=%GAME_COMMIT_PATH%\%WORK_NUMBER%\LubanConfig
 
 call gen_code_bin.bat
 
 echo ======== 开始生成版本文件 ========
 set configVersionOutPath=%ROOT_PATH%..\GenerateDatas
-set configVersion_UPLOAD_OutPath=%GAME_COMMIT_PATH%\%WORK_NUMBER%\%PLATFORM%
+set configVersion_UPLOAD_OutPath=%GAME_COMMIT_PATH%\%WORK_NUMBER%
 set OUTPUT_DATA_PATH=%WORKSPACE%\GenerateDatas\LubanConfig
 call ConfigVersion.exe %DATA_OUTPUT% %configVersionOutPath%
 echo ======== 生成版本文件结束 ========
@@ -25,6 +25,6 @@ echo ======== 开始复制Data文件 ========
 xcopy /s/y/i "%configVersionOutPath%\*.xml" "%configVersion_UPLOAD_OutPath%" 
 echo ======== 复制Data文件结束 ========
 
-start %configVersion_UPLOAD_OutPath%
+::start %configVersion_UPLOAD_OutPath%
 
 pause
