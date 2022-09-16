8e3588871440d5c2a4af8650bcd9f0a6c7b7eec5
it_Life <652597991@qq.com>
2022-09-16 19:29:04 +0800

    sync 同步HybridCLR

---
+++ Packages/com.focus-creative-games.hybridclr_unity/Editor/HybridCLRGlobalSettings.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditorInternal;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GlobalSettings", menuName = "HybridCLR/GlobalSettings")]
+public class HybridCLRGlobalSettings : ScriptableObject
+{
+    public bool enable = true;
+
+    public bool cloneFromGitee = true; // false 则从github上拉取
+
+    public string BuildCacheDir = "HybridCLRBuildCache"; //
+
+    public AssemblyDefinitionAsset[] hotfixAssemblyDefinitions;
+
+    public string[] hotfixAssemblies;
+
+    public string[] AOTMetadataDlls;
+
+    public string hotfixDllOutputDir = "HotFixDlls";
+
+    public string hybridCLRDataDir = "HybridCLRData";
+
+    public string strippedAssemblyDir = "AssembliesPostIl2CppStrip";
+}
