0f8e95336c45668c2f9fa982fd23c6685ccc4e22
AlanDu <42331817+It-Life@users.noreply.github.com>
2022-10-08 22:59:37 +0800

    Delete Assets/Deer/Scripts/HotFix directory

--- Assets/Deer/Scripts/HotFix/HotFixCommon/Definition/Constant/ProtoEventConstantName.cs
+++
@@ -1,7 +0,0 @@
-//-----------Auto Generate ByTools-----------
-//-----------Time:2022/9/28 17:38:14-----------
-//-----------Don't change-----------
-public static class ProtoEventName
-{
-}
-//-----------End-----------
--- Assets/Deer/Scripts/HotFix/HotFixFramework/Runtime/Component/SpriteCollectionExtension/StringSpriteDictionary.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-namespace UGFExtensions
-{
-    [Serializable]
-    public class StringSpriteDictionary : SerializableDictionary<string, Sprite> {}
-}
