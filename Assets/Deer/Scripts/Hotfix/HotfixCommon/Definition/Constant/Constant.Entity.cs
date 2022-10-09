2202abf03438782a434bb32c4486a65f8c454f0c
it_Life <652597991@qq.com>
2022-10-09 15:42:27 +0800

    change ignorecase

---
+++ Assets/Deer/Scripts/Hotfix/HotfixCommon/Definition/Constant/Constant.Entity.cs
@@ -0,0 +1,27 @@
+// ================================================
+//描 述:
+//作 者:杜鑫
+//创建时间:2022-06-17 15-50-10
+//修改作者:杜鑫
+//修改时间:2022-06-17 15-50-10
+//版 本:0.1 
+// ===============================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static partial class Constant 
+{
+    public static class EntityGroup
+    {
+        public const string Effect = "Effect";
+        public const string Elf = "Elf";
+        public const string Npc = "Npc";
+        public const string Landmark = "Landmark";
+        public const string Character = "Character";
+        public const string Ball = "Ball";
+    }
+}
+
+
+
