2fea2063ecbf89051851bd48b086f8835be084de
AlanDu <652597991@qq.com>
2023-06-16 18:57:07 +0800

    feat:增加支持mac 环境proto配套工具

--- LubanTools/Proto/Deer_Gen_Proto.sh
+++ LubanTools/Proto/Deer_Gen_Proto.sh
@@ -1,20 +1,24 @@
-#!/bin/zsh
-cd %~dp0
-ROOT_PATH=%~dp0
+#!/bin/bash
+cd "$(dirname "$0")"
+ROOT_PATH=$(pwd)
 WORKSPACE=..
 echo =================start gen proto code=================
 pb_path=pb_message
 out_path=../../Assets/Deer/Scripts/Hotfix/HotfixCommon/Proto
-del /f /s /q %out_path%\*.*
-for /f "delims=" %%i in ('dir /b %pb_path%') do (
-echo ------------%%i start gen
-protoc -I=pb_message --csharp_out=%out_path% pb_message\%%i
-echo ------------%%i gen success
-)
+rm -f $out_path/*
+for file in $pb_path/*
+do
+  if [ -f "$file" ]; then
+    echo "------------${file##*/} start gen"
+    protoc -I=pb_message --csharp_out=$out_path $file
+    echo "------------${file##*/} gen success"
+  fi
+done
 echo =================end gen proto code=================
-GEN_PROTOBUFRESOLVER=%WORKSPACE%\Tools\ProtobufResolver\ProtobufResolver.dll
-INPUT_DATA_DIR=%ROOT_PATH%pb_message
-OUTEVENTPATH=%WORKSPACE%/../Assets/Deer/Scripts/HotFix/HotFixCommon/Definition/Constant
-%GEN_PROTOBUFRESOLVER% --input_data_dir %INPUT_DATA_DIR% --output_proto_dir %OUTEVENTPATH%
+GEN_PROTOBUFRESOLVER=$WORKSPACE/Tools/ProtobufResolver/ProtobufResolver.exe
+INPUT_DATA_DIR=$ROOT_PATH/pb_message
+OUTEVENTPATH=$WORKSPACE/../Assets/Deer/Scripts/HotFix/HotFixCommon/Definition/Constant
+$GEN_PROTOBUFRESOLVER --input_data_dir $INPUT_DATA_DIR --output_proto_dir $OUTEVENTPATH
 echo =================end gen proto event=================
+read -p "Press any key to continue..."
 
